fe4cd16a1a6218dd0d0619602b2c0bb9c0581fac
TimBoudewijns <51700403+TimBoudewijns@users.noreply.github.com>
2019-08-19 11:53:11 +0200

    completed

--- 04 - Array Cardio Day 1/script.js
+++ 04 - Array Cardio Day 1/script.js
@@ -1,18 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <title>Array Cardio 💪</title>
-</head>
-<body>
-  <p><em>Psst: have a look at the JavaScript Console</em> 💁</p>
-  <script>
-    // Get your shorts on - this is an array workout!
-    // ## Array Cardio Day 1
-
-    // Some data we can work with
-
-    const inventors = [
+const inventors = [
       { first: 'Albert', last: 'Einstein', year: 1879, passed: 1955 },
       { first: 'Isaac', last: 'Newton', year: 1643, passed: 1727 },
       { first: 'Galileo', last: 'Galilei', year: 1564, passed: 1642 },
@@ -32,28 +18,76 @@
     // Array.prototype.filter()
     // 1. Filter the list of inventors for those who were born in the 1500's
 
+const fifteen = inventors.filter(inventor => (inventor.year >=1500 && inventor.year < 1600));
+
+console.table(fifteen);
+
     // Array.prototype.map()
     // 2. Give us an array of the inventors' first and last names
+
+const firstLastName = inventors.map(inventor => `${inventor.first} ${inventor.last}`);
+
+console.log(firstLastName);
 
     // Array.prototype.sort()
     // 3. Sort the inventors by birthdate, oldest to youngest
 
+const ordered = inventors.sort((a, b) => a.year > b.year ? 1 : -1);
+
+console.table(ordered);
+
     // Array.prototype.reduce()
     // 4. How many years did all the inventors live?
 
+const totalYears = inventors.reduce((total, inventor) => {
+  return total + (inventor.passed - inventor.year);
+}, 0);
+
+console.log(totalYears);
+
     // 5. Sort the inventors by years lived
+
+const oldest = inventors.sort(function(a,b) {
+  const firstPerson = a.passed - a.year;
+  const secondPerson = b.passed - b.year;
+  return firstPerson > secondPerson ? 1 : -1;
+
+});
+
+console.table(oldest);
 
     // 6. create a list of Boulevards in Paris that contain 'de' anywhere in the name
     // https://en.wikipedia.org/wiki/Category:Boulevards_in_Paris
 
+// const category = document.querySelector('.mw-category');
+// const links = Array.from(category.querySelectorAll('a'));
+// const title = links
+//               .map(links => links.textContent)
+//               .filter(streetName => streetName.includes('de'));
+
+// console.log(title);
 
     // 7. sort Exercise
     // Sort the people alphabetically by last name
+
+const peopleSort = people.sort((firstOne, nextOne) => {
+  const [aLast, aFirst] = firstOne.split(', ');
+  const [bLast, bFirst] = nextOne.split(', ');
+  return aLast > bLast ? 1 : -1;
+});
+
+console.log(peopleSort);
 
     // 8. Reduce Exercise
     // Sum up the instances of each of these
     const data = ['car', 'car', 'truck', 'truck', 'bike', 'walk', 'car', 'van', 'bike', 'walk', 'car', 'van', 'car', 'truck' ];
 
-  </script>
-</body>
-</html>
+const transportation = data.reduce(function(obj, item) {
+  if (!obj[item]) {
+    obj[item] = 0;
+  }
+    obj[item]++;
+    return obj;
+}, {})
+
+console.log(transportation);
